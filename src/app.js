d8fdad02403d073e292aa770a00524cbfedeef41
neutrino-ashutosh <neutrino.ashutosh@gmail.com>
2025-08-13 10:29:40 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
 
-app.use("/profile",(req, res) => {
-  res.send("hello from the server");
-})
+// app.use("/profile",(req, res) => {
+//   res.send("hello from the server");
+// })
 
-app.use("/test",(req, res) => {
-  res.send("hello from the test server");
+// app.use("/test",(req, res) => {
+//   res.send("hello from the test server");
+// })
+
+app.get("/user" , (req, res) => {
+  res.send("data fetched successfully");
+});
+
+app.post("/user" , (req, res) => {
+  res.send("data posted successfully");
+});
+
+app.delete("/user" , (req, res) => {
+  res.send("data deleted successfully");
+});
+
+app.use("/",(req, res) => {
+  res.send("welcome on server");
 })
 
 app.listen(3000 , () => {
