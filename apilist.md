55796afbeecc205eea0f893a53f311d5ea7e5a17
neutrino-ashutosh <neutrino.ashutosh@gmail.com>
2025-08-23 16:01:13 +0530

    added another connection related api [accepted/rejected]

--- apilist.md
+++ apilist.md
@@ -13,10 +13,10 @@
 - PATCH /profile/pasword
 
 ## ConnectionRequestRouter
-- POST /request/send/interested:userId
-- POST /request/send/ignored/:userId
-- POST /request/review/accepted/:requestId
-- POST /request/review/rejected/:requestId
+- POST /request/send/:status/:userId
+
+- POST /request/review/:status/:requestId
+
 
 ## userRouter
 - GET /user/connections
--- src/routes/requests.js
+++ src/routes/requests.js
@@ -4,6 +4,7 @@ const requestRouter = express.Router();
 const {userAuth} = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest")
 const User = require("../models/user");
+const { connection } = require("mongoose");
 
 
 requestRouter.post(
@@ -57,5 +58,39 @@ requestRouter.post(
     }
   }
 );
+
+requestRouter.post(
+  "/request/review/:status/:requestId" ,
+  userAuth ,
+  async (req,res) =>{
+    try{
+      const loggedInUser = req.user;
+      const {status, requestId} = req.params ;
+
+      const allowedStatus = ["accepted", "rejected"];
+      if(!allowedStatus.includes(status)){
+        return res.status(400).json({message : "status is not allowed"});
+      }
+
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      })
+
+      if(!connectionRequest) {
+        return res.status(400).json({message : "connection request not found"});
+      }
+
+      connectionRequest.status = status;
+      const data = await connectionRequest.save();
+
+      res.json({message : "connection request " + status, data });
+
+    } catch(err){
+      res.status(400).send("Error: " + err.message);
+    }
+  }
+)
 
 module.exports = requestRouter;
