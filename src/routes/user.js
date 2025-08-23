35334a6be3cb3f56a821f7258204e361c2a127f3
neutrino-ashutosh <neutrino.ashutosh@gmail.com>
2025-08-23 17:40:02 +0530

    added feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,8 +2,9 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const { connection } = require("mongoose");
 
-const USER_SAFE_DATA = "firstName lastName photourl age gender about skills";
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
 //  Get all the pending connection request for the logged in user
 userRouter.get("/user/requests/received", userAuth ,async (req, res) =>{
@@ -50,6 +51,41 @@ userRouter.get("/user/connections", userAuth, async(req,res) =>{
 
   } catch (err) {
     req.statusCode(400).send("message: "+ err.message);
+  }
+})
+
+userRouter.get("/feed", userAuth , async (req, res) => {
+  try {
+    // user should see all the user cards except
+    // his own card
+    // his connections
+    // ignored people
+    // already sent the connection request
+
+    const loggedInUser = req.user ;
+
+    // find all connection requests (sent + received)
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId : loggedInUser._id}, {toUserId : loggedInUser._id}],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach(req => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed)}},
+        { _id: { $ne: loggedInUser._id }}, 
+      ]
+    }).select(USER_SAFE_DATA);
+
+    res.send(connectionRequests)
+
+  } catch (err) {
+    res.status(400).json({message : err.message});
   }
 })
 
