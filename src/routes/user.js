bc33dda14cf5ed275992dd975c84ad24fe9b8e09
neutrino-ashutosh <neutrino.ashutosh@gmail.com>
2025-08-23 16:40:47 +0530

    added an api for getting list of connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,8 @@ const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
+const USER_SAFE_DATA = "firstName lastName photourl age gender about skills";
+
 //  Get all the pending connection request for the logged in user
 userRouter.get("/user/requests/received", userAuth ,async (req, res) =>{
   try{
@@ -13,7 +15,7 @@ userRouter.get("/user/requests/received", userAuth ,async (req, res) =>{
       status:" interested",
     }).populate(
       "fromUserId", 
-      ["firstName", "lastName", "photoUrl", "age", "about"]
+      USER_SAFE_DATA
     );
 
     res.json({
@@ -22,6 +24,26 @@ userRouter.get("/user/requests/received", userAuth ,async (req, res) =>{
     })
   }catch(err){
     req.statusCode(400).send("Error : " + err.message);
+  }
+})
+
+userRouter.get("/user/connections", userAuth, async(req,res) =>{
+  try{
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        {toUserId : loggedInUser._id, status: "accepteed"},
+        {fromUserId : loggedInUser._id, status: "accepteed"}
+      ],
+    }).populate("fromUserId", USER_SAFE_DATA );
+
+    const data = connectionRequests.map((row) => row.fromUserId);
+
+    res.json({ data : connectionRequests});
+
+  } catch (err) {
+    req.statusCode(400).send("message: "+ err.message);
   }
 })
 
