73a4255e1fc51e926fb78fa3aa494a52e1cd1dd0
neutrino-ashutosh <neutrino.ashutosh@gmail.com>
2025-08-27 23:57:10 +0530

    added a way to get the token back on client when user is opting a signup option on the web so that we can directly redirect them on profile page

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -22,12 +22,18 @@ authRouter.post("/signup" , async (req, res) => {
         firstName, 
         lastName,
         emailId,
-        password : passwordHash
+        password : passwordHash,
       }
     );
 
-    await user.save();
-    res.send("user added successfully");
+    const savedUser =  await user.save();
+    const token = await savedUser.getJWT();
+
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+      })
+
+    res.json({message : "user added successfully", data: savedUser});
   } catch(err) {
     res.status(400).send("error : " + err.message);
   }
